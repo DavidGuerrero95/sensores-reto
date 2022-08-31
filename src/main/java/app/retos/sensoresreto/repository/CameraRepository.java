179e6ac78fb30d3c5665d9735bf5f8eb942ef698
santiago <davids.guerrero@udea.edu.co>
2022-08-31 14:14:26 -0500

    Cambios

--- src/main/java/app/retos/sensoresreto/repository/CameraRepository.java
+++ src/main/java/app/retos/sensoresreto/repository/CameraRepository.java
@@ -2,7 +2,6 @@ package app.retos.sensoresreto.repository;
 
 
 import app.retos.sensoresreto.models.Camera;
-import app.retos.sensoresreto.models.SmartPost;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
