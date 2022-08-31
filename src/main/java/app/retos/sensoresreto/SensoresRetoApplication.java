e62e76c3d72d1423dcb081e95697cc2a48349245
santiago <davids.guerrero@udea.edu.co>
2022-08-31 07:03:51 -0500

    Initial commit

---
+++ src/main/java/app/retos/sensoresreto/SensoresRetoApplication.java
@@ -0,0 +1,13 @@
+package app.retos.sensoresreto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensoresRetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensoresRetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/retos/sensoresreto/SensoresRetoApplicationTests.java
@@ -0,0 +1,13 @@
+package app.retos.sensoresreto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensoresRetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
