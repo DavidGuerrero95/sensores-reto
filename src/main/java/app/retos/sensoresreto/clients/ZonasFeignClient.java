6a50c46ddd0fecb6c701a3edd0217f3810366246
Santiago <davids.guerrero@udea.edu.co>
2022-08-31 19:48:58 -0500

    cambios

---
+++ src/main/java/app/retos/sensoresreto/clients/ZonasFeignClient.java
@@ -0,0 +1,17 @@
+package app.retos.sensoresreto.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(name = "zonas-reto")
+public interface ZonasFeignClient {
+
+    @PostMapping("/events/crear/")
+    Integer crearZonesPosts(@RequestParam("idEvents") Integer idPosts,
+                            @RequestParam("location") List<Double> location);
+
+
+}
